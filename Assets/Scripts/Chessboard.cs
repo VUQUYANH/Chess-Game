4a5f1512d89ad253585a7e542dd6bbb5815128ca
Vũ Quý Anh <vqaanh@gmail.com>
2025-02-07 23:12:31 +0700

    update

--- Assets/Scripts/Chessboard.cs
+++ Assets/Scripts/Chessboard.cs
@@ -13,19 +13,18 @@ public class Chessboard : MonoBehaviour
     [SerializeField] private Material[] teamMaterials;
 
     // LOGIC
+    private ChessPiece[,] chessPieces;
     private const int TILE_COUNT_X = 8;
     private const int TILE_COUNT_Y = 8;
     private GameObject[,] tiles;
     private Camera currentCamera;
     private Vector2Int currentHover;
     private Vector3 bounds;
-    private ChessPiece[,] chessPiece;
 
     private void Awake()
     {
         GenerateAllTiles(tilleSize, TILE_COUNT_X, TILE_COUNT_Y);
         SpawnAllPieces();
-        PositionAllPiece();
     }
 
     private void Update()
@@ -90,9 +89,9 @@ public class Chessboard : MonoBehaviour
 
         Vector3[] vertices = new Vector3[4];
         vertices[0] = new Vector3(x * tileSize, yOffset, y * tileSize) - bounds;
-        vertices[1] = new Vector3(x * tileSize, yOffset, (y + 1) * tileSize) - bounds;
+        vertices[1] = new Vector3(x * tileSize, yOffset, (y+1) * tileSize) - bounds;
         vertices[2] = new Vector3((x+1) * tileSize, yOffset, y* tileSize) - bounds;
-        vertices[3] = new Vector3((x + 1) * tileSize, yOffset, (y + 1) * tileSize) - bounds;
+        vertices[3] = new Vector3((x+1) * tileSize, yOffset, (y+1) * tileSize) - bounds;
 
         int[] tris = new int[] { 0, 1, 2, 1, 3, 2 };
 
@@ -106,63 +105,56 @@ public class Chessboard : MonoBehaviour
         return tileObject;
     }
 
-    //Spawing of the pieces
+    // Spawing of the pieces
     private void SpawnAllPieces()
     {
-        chessPiece = new ChessPiece[TILE_COUNT_X, TILE_COUNT_Y];
-        chessPiece[0, 0] = SpawnSinglePiece(ChessPieceType.Rook, 0);
-        chessPiece[0, 1] = SpawnSinglePiece(ChessPieceType.Knight, 0);
-        chessPiece[0, 2] = SpawnSinglePiece(ChessPieceType.Bishop, 0);
-        chessPiece[0, 3] = SpawnSinglePiece(ChessPieceType.Queen, 0);
-        chessPiece[0, 4] = SpawnSinglePiece(ChessPieceType.King, 0);
-        chessPiece[0, 5] = SpawnSinglePiece(ChessPieceType.Bishop, 0);
-        chessPiece[0, 6] = SpawnSinglePiece(ChessPieceType.Knight, 0);
-        chessPiece[0, 7] = SpawnSinglePiece(ChessPieceType.Rook, 0);
+        chessPieces = new ChessPiece[TILE_COUNT_X ,TILE_COUNT_Y];
+
+        int whiteTeam = 0, blackTeam = 1;
+
+        //White team
+        chessPieces[0, 0] = SpawnSinglePiece(ChessPieceType.Rook, whiteTeam);
+        chessPieces[1, 0] = SpawnSinglePiece(ChessPieceType.Knight, whiteTeam);
+        chessPieces[2, 0] = SpawnSinglePiece(ChessPieceType.Bishop, whiteTeam);
+        chessPieces[3, 0] = SpawnSinglePiece(ChessPieceType.Queen, whiteTeam);
+        chessPieces[4, 0] = SpawnSinglePiece(ChessPieceType.King, whiteTeam);
+        chessPieces[5, 0] = SpawnSinglePiece(ChessPieceType.Bishop, whiteTeam);
+        chessPieces[6, 0] = SpawnSinglePiece(ChessPieceType.Knight, whiteTeam);
+        chessPieces[7, 0] = SpawnSinglePiece(ChessPieceType.Rook, whiteTeam);
         for (int i = 0; i < TILE_COUNT_X; i++)
-        {
-            chessPiece[1, i] = SpawnSinglePiece(ChessPieceType.Pawn, 0);
-        }
-        chessPiece[7, 0] = SpawnSinglePiece(ChessPieceType.Rook, 1);
-        chessPiece[7, 1] = SpawnSinglePiece(ChessPieceType.Knight, 1);
-        chessPiece[7, 2] = SpawnSinglePiece(ChessPieceType.Bishop, 1);
-        chessPiece[7, 3] = SpawnSinglePiece(ChessPieceType.Queen, 1);
-        chessPiece[7, 4] = SpawnSinglePiece(ChessPieceType.King, 1);
-        chessPiece[7, 5] = SpawnSinglePiece(ChessPieceType.Bishop, 1);
-        chessPiece[7, 6] = SpawnSinglePiece(ChessPieceType.Knight, 1);
-        chessPiece[7, 7] = SpawnSinglePiece(ChessPieceType.Rook, 1);
+            chessPieces[i, 1] = SpawnSinglePiece(ChessPieceType.Pawn, whiteTeam);
+
+        //Black team
+        chessPieces[0, 7] = SpawnSinglePiece(ChessPieceType.Rook, blackTeam);
+        chessPieces[1, 7] = SpawnSinglePiece(ChessPieceType.Knight, blackTeam);
+        chessPieces[2, 7] = SpawnSinglePiece(ChessPieceType.Bishop, blackTeam);
+        chessPieces[3, 7] = SpawnSinglePiece(ChessPieceType.Queen, blackTeam);
+        chessPieces[4, 7] = SpawnSinglePiece(ChessPieceType.King, blackTeam);
+        chessPieces[5, 7] = SpawnSinglePiece(ChessPieceType.Bishop, blackTeam);
+        chessPieces[6, 7] = SpawnSinglePiece(ChessPieceType.Knight, blackTeam);
+        chessPieces[7, 7] = SpawnSinglePiece(ChessPieceType.Rook, blackTeam);
         for (int i = 0; i < TILE_COUNT_X; i++)
-        {
-            chessPiece[6, i] = SpawnSinglePiece(ChessPieceType.Pawn, 1);
-        }
+            chessPieces[i, 6] = SpawnSinglePiece(ChessPieceType.Pawn, blackTeam);
+
 
     }
-
     private ChessPiece SpawnSinglePiece(ChessPieceType type, int team)
     {
         ChessPiece cp = Instantiate(prefabs[(int)type - 1], transform).GetComponent<ChessPiece>();
+
         cp.type = type;
         cp.team = team;
-        cp.mesh.material = teamMaterials[team];
-        cp.transform.localScale = new Vector3(cp.transform.localScale.x * cp.desiredScale.x, cp.transform.localScale.y * cp.desiredScale.y, cp.transform.localScale.z * cp.desiredScale.z);
+        cp.GetComponent<MeshRenderer>().material = teamMaterials[team];
+
         return cp;
-
     }
 
-    private void PositionAllPiece()
+    // Positioning
+    private void PositionAllPieces()
     {
-        for (int x = 0; x < TILE_COUNT_X; x++)
-            for (int y = 0; y < TILE_COUNT_Y; y++)
-                if (chessPiece[x, y] != null)
-                    PositionSinglePiece(x, y, true);
-    }
-    private void PositionSinglePiece(int x, int y, bool force = false)
-    {
-        chessPiece[x, y].currentX = x;
-        chessPiece[x, y].currentX = y;
-        chessPiece[x, y].transform.position = GetTileCenter(x,y);
+
     }
 
-    private Vector3 GetTileCenter(int x, int y) => new Vector3(x* tilleSize,yOffset,y* tilleSize) - bounds + new Vector3(tilleSize/2,0,tilleSize/2);
 
     // Operations
     private Vector2Int LookupTileIndex(GameObject hitInfo)
