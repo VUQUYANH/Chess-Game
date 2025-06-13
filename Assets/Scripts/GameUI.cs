96bd0523949998aabf33773b9dcac1f6b1363ed5
Vũ Quý Anh <vqaanh@gmail.com>
2025-06-13 22:45:42 +0700

    update

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -2,5 +2,36 @@ using UnityEngine;
 
 public class GameUI : MonoBehaviour
 {
-    
+    public static GameUI Instance { set; get; }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    //Buttons
+    public void OnLocalGameButton()
+    {
+        Debug.Log("OnLocalGameButton");
+    }
+    public void OnOnlineGameButton()
+    {
+        Debug.Log("OnOnlineGameButton");
+    }
+    public void OnOnlineHostButton()
+    {
+        Debug.Log("OnOnlineHostButton");
+    }
+    public void OnOnlineConnectButton()
+    {
+        Debug.Log("OnOnlineConnectButton");
+    }
+    public void OnOnlineBackButton()
+    {
+        Debug.Log("OnOnlineBackButton");
+    }
+    public void OnHostBackButton()
+    {
+        Debug.Log("OnOnlineBackButton");
+    }
 }
